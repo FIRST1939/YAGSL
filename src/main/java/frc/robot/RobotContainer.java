c534bc5d711d2d04fc02739eb570ba570b0c6943
1939DriverStation <barstowrobotics1939@gmail.com>
2024-02-01 21:55:11 -0600

    Functional Swerve Code
    This code is functional and working for the Systems Test Base.

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,53 @@
+package frc.robot;
+
+import java.io.IOException;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.lib.Controller;
+import frc.robot.commands.swerve.BrakeMode;
+import frc.robot.commands.swerve.Drive;
+import frc.robot.commands.swerve.TrackAprilTags;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Swerve;
+import frc.robot.util.Alerts;
+import frc.robot.util.Constants;
+
+public class RobotContainer {
+
+    private Swerve swerve;
+    private Limelight limelight;
+    private CommandXboxController driverOne;
+
+    public RobotContainer () {
+
+        try { this.swerve = new Swerve(); } 
+        catch (IOException ioException) { Alerts.swerveInitialized.set(true); }
+
+        this.limelight = new Limelight();
+        this.driverOne = new Controller(0);
+
+        this.configureCommands();
+    }
+
+    private void configureCommands () {
+
+        this.driverOne.leftBumper().whileTrue(new RepeatCommand(new InstantCommand(this.swerve::lock, this.swerve)));
+
+        this.swerve.setDefaultCommand(new Drive(
+            this.swerve, 
+            () -> MathUtil.applyDeadband(-this.driverOne.getHID().getLeftY(), Constants.SwerveConstants.TRANSLATION_DEADBAND),
+            () -> MathUtil.applyDeadband(-this.driverOne.getHID().getLeftX(), Constants.SwerveConstants.TRANSLATION_DEADBAND), 
+            () -> MathUtil.applyDeadband(this.driverOne.getHID().getRightX(), Constants.SwerveConstants.OMEGA_DEADBAND), 
+            () -> this.driverOne.getHID().getPOV()
+        ));
+
+        this.limelight.setDefaultCommand(new TrackAprilTags(this.swerve, this.limelight));
+    }
+
+    public Command getAutonomousCommand () { return this.swerve.getAutonomousCommand(); }
+    public void setBrakeMode (boolean brake) { new BrakeMode(this.swerve, brake).schedule(); }
+}
