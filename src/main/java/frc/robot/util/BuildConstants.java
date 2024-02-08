98eb13c9cd77e634720efd913f3b74752ed4c55b
StarbuckBarista <carlitos.salazar@protonmail.com>
2024-02-08 00:54:17 -0600

    Adjust Translation Directions

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "YAGSL";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 8;
-  public static final String GIT_SHA = "2fc8947d334962af5508641b5ec73f2f686517af";
-  public static final String GIT_DATE = "2024-02-03 14:09:42 CST";
-  public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-02-08 00:40:04 CST";
-  public static final long BUILD_UNIX_TIME = 1707374404747L;
+  public static final int GIT_REVISION = 9;
+  public static final String GIT_SHA = "59838db2a5c306686f0c266974f52b5ff97d650a";
+  public static final String GIT_DATE = "2024-02-08 00:49:26 CST";
+  public static final String GIT_BRANCH = "allianceOrientated";
+  public static final String BUILD_DATE = "2024-02-08 00:52:11 CST";
+  public static final long BUILD_UNIX_TIME = 1707375131299L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
