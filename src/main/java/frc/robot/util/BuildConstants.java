4d682bb28703f12d425134a4c2891b3820f5b424
1939DriverStation <barstowrobotics1939@gmail.com>
2024-02-26 18:23:57 -0600

    Update Angle PID Values

--- src/main/java/frc/robot/util/BuildConstants.java
+++ src/main/java/frc/robot/util/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "YAGSL";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "d3b1d8f42a317027e9383c65458ec2f0b689f834";
-  public static final String GIT_DATE = "2024-02-08 01:02:47 CST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "558683cd1b04454321b19ea8ca81bdfeb3cb7d73";
+  public static final String GIT_DATE = "2024-02-08 11:03:54 CST";
   public static final String GIT_BRANCH = "allianceOriented";
-  public static final String BUILD_DATE = "2024-02-08 11:03:42 CST";
-  public static final long BUILD_UNIX_TIME = 1707411822668L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-02-26 18:22:01 CST";
+  public static final long BUILD_UNIX_TIME = 1708993321112L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
